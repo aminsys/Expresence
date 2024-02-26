31e886939d3b48c5bb3da39518c8cc147b0bb901
aminsys <amin.yassin@live.com>
2024-02-26 10:27:18 +0000

    Add publicPath function and set it to the CI_PAGES_URL if it exists, otherwise set it to '/'.

--- expresence/vite.config.js
+++ expresence/vite.config.js
@@ -3,21 +3,35 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+function publicPath () {
+  if (process.env.CI_PAGES_URL) {
+    return new URL(process.env.CI_PAGES_URL).pathname
+  } else {
+    return '/'
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
   ],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+
   server: {
     host: true,
     port: 4173,
     watch: {
       usePolling: true
     }
-  }
+  },
+  
+  transpileDependencies: true,
+  publicPath: publicPath(),
+  outputDir: 'public'
 })
