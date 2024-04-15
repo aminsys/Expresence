dca893240fa2c88bac70e132c9338b2af61ff6db
aminsys <amin.yassin@live.com>
2024-04-15 12:41:42 +0000

    Change API URL

--- src/stores/DataStore.js
+++ src/stores/DataStore.js
@@ -13,14 +13,14 @@ export const useDataStore = defineStore("DataStore", {
     actions: {
         async populateData(fromDate, toDate, header){
            
-                this.data = (await axios.get("https://expresence-api-2-api-service.azure-api.net/persons-with-days?fromDate="+fromDate+"&toDate="+toDate, header)).data;         
+                this.data = (await axios.get("https://expresence-api-220240404101112.azurewebsites.net/persons-with-days?fromDate="+fromDate+"&toDate="+toDate, header)).data;         
             },
         async addNewUser(user, header){
-            (await axios.post("https://expresence-api-2-api-service.azure-api.net/persons", user, header));
+            (await axios.post("https://expresence-api-220240404101112.azurewebsites.net/persons", user, header));
         },
 
         async getAllPersons(header){
-            this.persons = (await axios.get("https://expresence-api-2-api-service.azure-api.net/persons", header)).data;
+            this.persons = (await axios.get("https://expresence-api-220240404101112.azurewebsites.net/persons", header)).data;
         }
     }
 
